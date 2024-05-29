bb1240b17de32c92eb6a837b25d6601c7d86e21b
Ruwen-Yi <ruwen.yi@hotmail.com>
2024-05-29 14:08:45 +0930

    add comments

--- app/dashboard/invoices/error.tsx
+++ app/dashboard/invoices/error.tsx
@@ -21,7 +21,7 @@ export default function Error({
       <button
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
         onClick={
-          // attempt to recover by trying to re-render the invoices route
+          // attempt to recover by trying to re-render the dashboard/invoices route
           () => reset()
         }
       >
