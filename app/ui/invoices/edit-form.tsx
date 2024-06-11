37ad2f610a16cd18355a4ec2282f40c9998c037e
Ruwen-Yi <ruwen.yi@hotmail.com>
2024-06-11 15:31:18 +0930

    display amount and invocie status error in edit form

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -78,8 +78,17 @@ export default function EditInvoiceForm({
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby='amount-error'
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+            <div id='amount-error'>
+              {state.errors?.amount && 
+                state.errors.amount.map((error: string) => (
+                    <p className="mt-2 text-sm text-red-500" key={error}>
+                        {error}
+                    </p>
+                ))}
             </div>
           </div>
         </div>
@@ -99,6 +108,7 @@ export default function EditInvoiceForm({
                   value="pending"
                   defaultChecked={invoice.status === 'pending'}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby='invoice-status-error'
                 />
                 <label
                   htmlFor="pending"
@@ -115,6 +125,7 @@ export default function EditInvoiceForm({
                   value="paid"
                   defaultChecked={invoice.status === 'paid'}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby='invoice-status-error'
                 />
                 <label
                   htmlFor="paid"
@@ -122,6 +133,14 @@ export default function EditInvoiceForm({
                 >
                   Paid <CheckIcon className="h-4 w-4" />
                 </label>
+              </div>
+              <div id='invoice-status-error'>
+                {state.errors?.status &&
+                  state.errors.status.map((error: string) => (
+                    <p className="mt-2 text-sm text-red-500" key={error}>
+                        {error}
+                    </p>
+                  ))}
               </div>
             </div>
           </div>
